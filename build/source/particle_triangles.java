a06ab6250324fe208f2340289f126df8468dfd2f
Jinju Jang <jinjubread@gmail.com>
2017-06-03 22:55:20 +1000

    아톰에서 돌리니까 이런 반응이 나오는군.

--- build/source/particle_triangles.java
+++ build/source/particle_triangles.java
@@ -1,11 +1,30 @@
-import processing.opengl.*;
-import processing.pdf.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import processing.opengl.*; 
+import processing.pdf.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class particle_triangles extends PApplet {
 
 
-/* 목표
-6. 움직임을 부드럽게...
-7. 선의 갯수를 반복되지 않도록 줄여본다.
-8. z 축의 개념을 도입해서 (가운데 점?) 볼록 하게 만들어본다!
+
+
+
+/* \ubaa9\ud45c
+6. \uc6c0\uc9c1\uc784\uc744 \ubd80\ub4dc\ub7fd\uac8c...
+7. \uc120\uc758 \uac2f\uc218\ub97c \ubc18\ubcf5\ub418\uc9c0 \uc54a\ub3c4\ub85d \uc904\uc5ec\ubcf8\ub2e4.
+8. z \ucd95\uc758 \uac1c\ub150\uc744 \ub3c4\uc785\ud574\uc11c (\uac00\uc6b4\ub370 \uc810?) \ubcfc\ub85d \ud558\uac8c \ub9cc\ub4e4\uc5b4\ubcf8\ub2e4!
 */
 
 PVector midPoint;
@@ -28,10 +47,10 @@ float midPointX, midPointY;
 float areaX, areaY, areaTotal;
 
 
-void setup() {
-  size(800, 600, OPENGL);
-  pixelDensity(displayDensity());
-  smooth(8);
+public void setup() {
+  
+  
+  
   colorMode(HSB,360,100,100,1);
   background(0, 0, 100, 1);
 
@@ -55,14 +74,14 @@ void setup() {
 
 }
 
-void draw() {
+public void draw() {
   fill(0, 0, 100, 1);
   noStroke();
   rect(0, 0, width, height);
 
   int index = 0;
-  float fading = 0.9;
-  // 뭔가 삼각형을 만들려면 이렇게 해야하는 걸까...
+  float fading = 0.9f;
+  // \ubb54\uac00 \uc0bc\uac01\ud615\uc744 \ub9cc\ub4e4\ub824\uba74 \uc774\ub807\uac8c \ud574\uc57c\ud558\ub294 \uac78\uae4c...
 
   for(int i = 0; i < particles.size(); i++){
     pushMatrix();
@@ -91,7 +110,7 @@ void draw() {
           midPointY = (particle1.y + particle2.y + particle3.y) / 3;
           // vP2.add(vP1);
 
-          // 넓이로 가자...
+          // \ub113\uc774\ub85c \uac00\uc790...
           areaX = (particle1.x * particle2.y) + (particle2.x * particle3.y) + (particle3.x * particle1.y);
           areaY = (particle1.x * particle3.y) + (particle3.x * particle2.y) + (particle2.x * particle1.y);
           areaTotal = (areaX - areaY)/2;
@@ -118,7 +137,7 @@ void draw() {
 }
 
 public void styling(){
-  color c;
+  int c;
 
   pushStyle();
   strokeWeight(1);
@@ -127,14 +146,14 @@ public void styling(){
   // stroke(c);
 }
 
-public void triangleGen(float x1, float y1, float x2, float y2, float x3, float y3, color c, float colorOpacity){
+public void triangleGen(float x1, float y1, float x2, float y2, float x3, float y3, int c, float colorOpacity){
   pushStyle();
   fill(c, colorOpacity);
   triangle(x1, y1, x2, y2, x3, y3);
   popStyle();
 }
 
-// 색을...써보아요...
+// \uc0c9\uc744...\uc368\ubcf4\uc544\uc694...
 public int colorBlended(float fract, float h, float s, float b, float h2, float s2, float b2, float a){
   h2 = (h2 - h);
   s2 = (s2 - s);
@@ -147,7 +166,7 @@ class Particle {
   float x, y, r;
   float z;
   float randomiser;
-  color c;
+  int c;
   int i = 1, j = 1;
 
   Particle(){
@@ -155,16 +174,16 @@ class Particle {
     y = random(trim, randomBoundaryHeight);
     r = 4;
 
-    // 색은 랜덤으로 뽑자..
+    // \uc0c9\uc740 \ub79c\ub364\uc73c\ub85c \ubf51\uc790..
     randomiser = random(100);
     colorMode(HSB,360,100,100,1);
 
-    if(randomiser > 70) c = colorBlended(random(1), 178, 37, 92, 171, 68, 72, 0.8);
-    else if(randomiser > 30) c = colorBlended(random(1), 188, 37, 92, 171, 68, 72, 0.8);
-    else c = colorBlended(random(1), 161, 39, 94, 199, 96, 60, 0.8);
+    if(randomiser > 70) c = colorBlended(random(1), 178, 37, 92, 171, 68, 72, 0.8f);
+    else if(randomiser > 30) c = colorBlended(random(1), 188, 37, 92, 171, 68, 72, 0.8f);
+    else c = colorBlended(random(1), 161, 39, 94, 199, 96, 60, 0.8f);
   }
 
-  void display(){
+  public void display(){
     pushStyle();
     noStroke();
     fill(c);
@@ -172,7 +191,7 @@ class Particle {
     popStyle();
   }
 
-  void triangleGen(Particle p2, Particle p3){
+  public void triangleGen(Particle p2, Particle p3){
     pushStyle();
     noStroke();
 
@@ -185,9 +204,9 @@ class Particle {
     float midPointX = (x + x2 + x3) / 3;
     float midPointY = (y + y2 + y3) / 3;
 
-    color c1 = c;
-    color c2 = p2.c;
-    color c3 = p3.c;
+    int c1 = c;
+    int c2 = p2.c;
+    int c3 = p3.c;
 
     fill(c1);
     triangle(x, y, x2, y2, midPointX, midPointY);
@@ -201,40 +220,50 @@ class Particle {
     popStyle();
   }
 
-  void update()
+  public void update()
   {
-    // animating logic (처음엔 정방향이라서 재미가 없다!!!)
-    x = x + j*0.02;
-    y = y + i*0.02;
+    // animating logic (\ucc98\uc74c\uc5d4 \uc815\ubc29\ud5a5\uc774\ub77c\uc11c \uc7ac\ubbf8\uac00 \uc5c6\ub2e4!!!)
+    x = x + j*0.02f;
+    y = y + i*0.02f;
 
     if (y > randomBoundaryHeight - r) {
       i =- 1;
-      rotateX(PI/3.0 * i);
-      // 파란계열
-      c = colorBlended(random(1), 195, 80, 50, 210, 96, 100, 0.8);
+      rotateX(PI/3.0f * i);
+      // \ud30c\ub780\uacc4\uc5f4
+      c = colorBlended(random(1), 195, 80, 50, 210, 96, 100, 0.8f);
 
     }
     if (y < trim + r) {
       i = 1;
-      rotateX(PI/3.0 * -i);
-      // 사이안 계열
-      c = colorBlended(random(1), 190, 40, 75, 199, 96, 95, 0.8);
+      rotateX(PI/3.0f * -i);
+      // \uc0ac\uc774\uc548 \uacc4\uc5f4
+      c = colorBlended(random(1), 190, 40, 75, 199, 96, 95, 0.8f);
 
     }
 
     if (x > randomBoundary - r){
       j =- 1;
-      c = colorBlended(random(1), 181, 80, 75, 199, 96, 70, 0.8);
-      rotateY(PI/3.0 * j);
+      c = colorBlended(random(1), 181, 80, 75, 199, 96, 70, 0.8f);
+      rotateY(PI/3.0f * j);
 
     }
 
     if (x < trim + r) {
       j = 1;
-      // 녹색계??
-      c = colorBlended(random(1), 171, 50, 94, 199, 96, 40, 0.8);
-      rotateY(PI/3.0 * -j);
+      // \ub179\uc0c9\uacc4??
+      c = colorBlended(random(1), 171, 50, 94, 199, 96, 40, 0.8f);
+      rotateY(PI/3.0f * -j);
     }
 
+  }
+}
+  public void settings() {  size(800, 600, OPENGL);  smooth(8);  pixelDensity(displayDensity()); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "particle_triangles" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
